ecdd1ad69fad2fa9157a03ae7012b9d7937feccd
Gitagia-Tech <rosewambui8197@gmail.com>
2025-09-05 13:36:34 +0300

    new changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,11 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { ChatsModule } from './chats/chats.module';
-import { GroupsModule } from './groups/groups.module';
-import { MessagesModule } from './messages/messages.module';
-import { RealtimeModule } from './realtime/realtime.module';
-import { NotificationsModule } from './notifications/notifications.module';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MessageModule } from './message/message.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(process.env.MONGO_URI),
-    AuthModule,
-    UsersModule,
-    ChatsModule,
-    GroupsModule,
-    MessagesModule,
-    RealtimeModule,
-    NotificationsModule,
-  ],
+  imports: [MessageModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
-
