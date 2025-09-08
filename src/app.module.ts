f975e6b0eed39f8b48b6ace7b41c8ca283dfa1b7
Gitagia-Tech <rosewambui8197@gmail.com>
2025-09-08 16:33:46 +0300

    Adding schemas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MessageModule } from './message/message.module';
+import { MessagesModule } from './messages/messages.module';
 import { ChatsModule } from './chats/chats.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { MessagesModule } from './messages/messages.module';
-import { ChatsModule } from './chats/chats.module';
+
+
 
 @Module({
-  imports: [MessageModule, ChatsModule, MessagesModule, UsersModule, AuthModule],
+  imports: [MessagesModule, ChatsModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
